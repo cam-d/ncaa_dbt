a339414355327173c6e1e1925c25c6481aaf8b4a
cam-d <cameron.delapena@gmail.com>
2022-03-12 12:10:47 -0500

    fix mappings, feature calcs

--- models/features/features_base.sql
+++ models/features/features_base.sql
@@ -1,8 +1,9 @@
 /*
 Base template for the games, similar to prediction
+Union of NCAA tournament games and Conference Tournament Games
 */
 
-SELECT 
+(SELECT 
     game_Id
     , season
     , team_a 
@@ -10,4 +11,19 @@ SELECT
     , team_a_seed 
     , team_b_seed 
     , team_a_win 
+    , 'ncaa' AS tourney_type
 FROM {{ ref('base_tourney_games') }}
+)
+UNION ALL 
+(SELECT 
+    game_Id
+    , season
+    , team_a 
+    , team_b 
+    -- seeds not included for conf tournaments
+    , NULL AS team_a_seed
+    , NULL As team_b_seed 
+    , team_a_win 
+    , 'conference' AS tourney_type
+FROM {{ ref('base_conf_tourney_games') }}
+)
